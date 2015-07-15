d5d19145c8949d819581bd666dce207a8d53c848
Peter Cai <xqsx43cxy@126.com>
2015-07-16 07:27:27 +0800

    server: do not create objects for every packet
    Signed-off-by: Peter Cai <xqsx43cxy@126.com>

--- src/server.py
+++ src/server.py
@@ -39,24 +39,26 @@ def main_loop():
 			data = os.read(tunfd, 32767)
 
 			dst = data[16:20]
-			if dst in clients:
+			if dst in clients.keys():
 				udp.sendto(data, clients[dst]['ip'])
-
-				# Update the last active time
-				clients[dst]['time'] = time.time()
 			else:
 				logging.warn(dst + " not found")
 
-				clearClients()
+			clearClients()
 
 		if udpfd in r:
 			data, src = udp.recvfrom(32767)
 			os.write(tunfd, data)
 			#logging.info('connection from %s:%d' % src)
-			clients[data[12:16]] = {
-				'ip': src,
+                        c = data[12:16] # The source
+                        if c in clients.keys():
+                            clients[c]['time'] = time.time()
+                            clients[c]['ip'] = src
+                        else:
+		    	    clients[c] = {
+			    	'ip': src,
 				'time': time.time()
-			}
+		            }
 
 	# Oops, loop cancelled. Something goes wrong
 	utils.iptables_reset(config['virtual_ip'], config['output'])
