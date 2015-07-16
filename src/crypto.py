ef216cb194dddf9ce0e92163c037cad00a927b68
Peter Cai <xqsx43cxy@126.com>
2015-07-16 10:05:59 +0800

    Revert "crypto: do not base64 encode"
    This reverts commit 6e1a449da027cea8ce39891458f775aa99b509c6.

--- src/crypto.py
+++ src/crypto.py
@@ -13,16 +13,17 @@ def md5(key):
 	return m.digest()
 
 class AESCipher:
-	def __init__(self, key):
+	def __init__( self, key ):
 		self.key = key
 
-	def encrypt(self, raw):
+	def encrypt( self, raw ):
 		raw = pad(raw)
-		iv = Random.new().read(AES.block_size)
-		cipher = AES.new( self.key, AES.MODE_CBC, iv)
-		return iv + cipher.encrypt(raw)
+		iv = Random.new().read( AES.block_size )
+		cipher = AES.new( self.key, AES.MODE_CBC, iv )
+		return base64.b64encode( iv + cipher.encrypt( raw ) )
 
 	def decrypt( self, enc ):
+		enc = base64.b64decode(enc)
 		iv = enc[:16]
-		cipher = AES.new(self.key, AES.MODE_CBC, iv)
-		return unpad(cipher.decrypt(enc[16:]))
+		cipher = AES.new(self.key, AES.MODE_CBC, iv )
+		return unpad(cipher.decrypt( enc[16:] ))
